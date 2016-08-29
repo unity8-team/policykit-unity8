6da2b306a8f48d39a76d0e697c70b1597d1b7f27
Ted Gould <ted@gould.cx>
2016-08-29 13:05:42 -0500

    Make sure this is a variant truth

--- service/agent.cpp
+++ service/agent.cpp
@@ -49,7 +49,7 @@ Agent::Agent(const std::shared_ptr<AuthManager>& authmanager)
         g_variant_builder_init(&builder, G_VARIANT_TYPE_VARDICT);
         /* Makes us the fallback agent so that system settings can override
            when it is setting multiple password settings all at once. */
-        g_variant_builder_add_parsed(&builder, "{'fallback', true}");
+        g_variant_builder_add_parsed(&builder, "{'fallback', <true>}");
 
         /* Register it */
         GError* registererror = nullptr;
